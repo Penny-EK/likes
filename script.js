2a072d1a94c1830c1d0c170fc277f73fbd892b05
pekr0002 <pekr0002@stud.kea.dk>
2025-10-27 14:39:32 +0100

    OPG 2 VIRKER

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-let likes = 0;
-document.querySelector("button").addEventListener("click", () => {
-  console.log("knap klik");
-  likes++;
-  console.log(likes);
-  document.querySelector("button").textContent = `❤️ ${likes} Like`;
-  if (likes > 1) {
-    document.querySelector("button").textContent = `❤️ ${likes} Likes`;
-  }
-});
+// OPG 1
+
+// let likes = 0;
+
+// document.querySelector(".button").addEventListener("click", click);
+// //   console.log("knap klik");
+// //   likes++;
+// //   console.log(likes);
+// //   document.querySelector("button").textContent = `❤️ ${likes} Like`;
+// //   if (likes > 1) {
+// //     document.querySelector("button").textContent = `❤️ ${likes} Likes`;
+// //   }
+// // });
+
+// function click() {
+//   console.log("knap klik");
+//   likes++;
+//   console.log(likes, this);
+//   this.textContent = `❤️ ${likes} Like`;
+//   if (likes > 1) {
+//     this.textContent = `❤️ ${likes} Likes`;
+//   }
+// }
+
+// OPG 2
+
+// let likes = 0;
+
+// document.querySelectorAll(".button").forEach((button) => {
+//   button.addEventListener("click", click);
+// });
+
+// function click() {
+//   console.log("knap klik");
+//   likes++;
+//   console.log(likes, this);
+//   this.textContent = `❤️ ${likes} Like`;
+//   if (likes > 1) {
+//     this.textContent = `❤️ ${likes} Likes`;
+//   }
+// }
